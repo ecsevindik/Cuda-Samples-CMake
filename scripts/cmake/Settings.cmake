2fcfeafa182a19f8dbb7adb2be7a59e0c43955b0
Rulokat <mrcnsvndk@gmail.com>
2022-06-14 12:15:30 +0300

    override nvcc flags for tensorcoregemm sample

--- scripts/cmake/Settings.cmake
+++ scripts/cmake/Settings.cmake
@@ -38,7 +38,7 @@ macro(setupCompilerAndLinker)
                                               -gencode arch=compute_75,code=sm_75
                                               -gencode arch=compute_80,code=sm_80;
                                               -gencode arch=compute_86,code=sm_86;
-      )
+        )
       endif()
     endif()
    
